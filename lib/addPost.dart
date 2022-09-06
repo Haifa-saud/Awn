cc0e064c3b3df94c95c30e3f93496300ccf2eab3
reemswe <98765331+reemswe@users.noreply.github.com>
2022-09-07 00:43:38 +0300

    added upload image feature

--- lib/addPost.dart
+++ lib/addPost.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'CustomDatabase.dart';
 import 'firebase_options.dart';
 
 // Future<void> main() async {
