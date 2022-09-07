190cca9c88316b1ca7d9e66eeeec9ae7595484c0
Leana aldomaiji <leenaziz3491@gmail.com>
2022-09-08 01:55:03 +0300

    .

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,44 +8,175 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TextScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class TextScreen extends StatefulWidget {
+  const TextScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<TextScreen> createState() => _TextScreenState();
+}
+
+class _TextScreenState extends State<TextScreen> {
+  DateTime selectedDate = DateTime.now();
+  TimeOfDay selectedTime = TimeOfDay.now();
+  DateTime dateTime = DateTime.now();
+  bool showDate = false;
+  bool showTime = false;
+  bool showDateTime = false;
+
+  // Select for Date
+  Future<DateTime> _selectDate(BuildContext context) async {
+    final selected = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2025),
+    );
+    if (selected != null && selected != selectedDate) {
+      setState(() {
+        selectedDate = selected;
+      });
+    }
+    return selectedDate;
+  }
+
+  // Select for Time
+  Future<TimeOfDay> _selectTime(BuildContext context) async {
+    final selected = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+    );
+    if (selected != null && selected != selectedTime) {
+      setState(() {
+        selectedTime = selected;
+      });
+    }
+    return selectedTime;
+  }
+  // select date time picker
+
+  Future _selectDateTime(BuildContext context) async {
+    final date = await _selectDate(context);
+    if (date == null) return;
+
+    final time = await _selectTime(context);
+
+    if (time == null) return;
+    setState(() {
+      dateTime = DateTime(
+        date.year,
+        date.month,
+        date.day,
+        time.hour,
+        time.minute,
+      );
+    });
+  }
+
+  String getDate() {
+    // ignore: unnecessary_null_comparison
+    if (selectedDate == null) {
+      return 'select date';
+    } else {
+      return DateFormat('MMM d, yyyy').format(selectedDate);
+    }
+  }
+
+  String getDateTime() {
+    // ignore: unnecessary_null_comparison
+    if (dateTime == null) {
+      return 'select date timer';
+    } else {
+      return DateFormat('yyyy-MM-dd HH: ss a').format(dateTime);
+    }
+  }
+
+  String getTime(TimeOfDay tod) {
+    final now = DateTime.now();
+
+    final dt = DateTime(now.year, now.month, now.day, tod.hour, tod.minute);
+    final format = DateFormat.jm();
+    return format.format(dt);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Awn Requist')),
-        body: SingleChildScrollView(
-            child: Center(
-          child: Column(children: <Widget>[
-            Text('Awn Requist',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
-            AwnRequestForm(),
-            /*  Container(
-              color: Color.fromARGB(255, 222, 222, 222),
-              width: 300,
-              height: 300,
-              margin: EdgeInsets.only(bottom: 20, top: 20),
-              child: Center(child: Text("data2")),
+      appBar: AppBar(
+        title: const Text('Awn Requist'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Text('Please select the time and date',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
             ),
             Container(
-                color: Color.fromARGB(255, 222, 222, 222),
-                width: 300,
-                height: 300,
-                child: Center(child: Text("data2"))),*/
-          ]),
-        )));
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              margin: EdgeInsets.only(bottom: 20, top: 20),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectDate(context);
+                  showDate = true;
+                },
+                child: const Text('Date Picker'),
+              ),
+            ),
+            showDate ? Center(child: Text(getDate())) : const SizedBox(),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectTime(context);
+                  showTime = true;
+                },
+                child: const Text('Timer Picker'),
+              ),
+            ),
+            showTime
+                ? Center(child: Text(getTime(selectedTime)))
+                : const SizedBox(),
+            /*  Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  _selectDateTime(context);
+                  showDateTime = true;
+                },
+                child: const Text('Select Date and Time Picker'),
+              ),
+            ),
+            showDateTime
+                ? Center(child: Text(getDateTime()))
+                : const SizedBox(),*/
+            Column(children: <Widget>[
+              Text('',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600)),
+              AwnRequestForm(),
+            ]),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -63,7 +195,7 @@ class AwnRequestFormState extends State<AwnRequestForm> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            TextFormField(
+            /*TextFormField(
               decoration: const InputDecoration(
                 icon: Icon(Icons.schedule),
                 hintText: 'time',
@@ -84,7 +216,17 @@ class AwnRequestFormState extends State<AwnRequestForm> {
                 hintText: 'date',
                 labelText: 'date',
               ),
-            ),
+            ),*/
+            /*const SizedBox(height: 16),
+            ElevatedButton(
+                onPressed: () async {
+                  showDatePicker(
+                      context: context,
+                      initialDate: initialDate,
+                      firstDate: firstDate,
+                      lastDate: lastDate);
+                },
+                child: Text('select date')),*/
             TextFormField(
               decoration: const InputDecoration(
                 icon: Icon(Icons.schedule),
@@ -98,6 +240,13 @@ class AwnRequestFormState extends State<AwnRequestForm> {
                 hintText: 'describtion',
                 labelText: 'describtion',
               ),
+              keyboardType: TextInputType.datetime,
+              onChanged: (value) {},
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please Provide a discription';
+                }
+              },
             ),
             SizedBox(
               height: 10,
