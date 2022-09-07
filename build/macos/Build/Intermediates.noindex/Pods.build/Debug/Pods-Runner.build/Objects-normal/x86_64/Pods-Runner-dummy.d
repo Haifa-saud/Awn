68bf55cb31f486095458cef664a09f9659ce55e7
Wedd-H <Wedd-s-h@hotmail.com>
2022-09-07 15:19:19 +0300

    first commit

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/doodi/Desktop/444_Awn/awn/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/doodi/Desktop/444_Awn/awn/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/doodi/Desktop/444_Awn/awn/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
